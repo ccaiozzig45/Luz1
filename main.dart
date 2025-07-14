a4308f726696d47900047e6915c697389b7e1622
ccaiozzig45 <ccaiozzig@gmail.com>
2025-07-14 01:44:23 -0400

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,2 +1,36 @@
-// Punto de entrada de la app Luz
-void main() => runApp(LuzApp());
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Luz App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Luz"),
+      ),
+      body: const Center(
+        child: Text("Bienvenido a Luz"),
+      ),
+    );
+  }
+}
